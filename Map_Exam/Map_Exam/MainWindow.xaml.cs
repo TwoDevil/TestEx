dbb75ddf5f857468966a201556b471fb3638e188
Diman96 <nde96m@gmail.com>
2016-10-19 15:10:00 +0300

    Test

---
+++ Map_Exam/Map_Exam/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+//тест
+namespace Map_Exam
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OracleConnection oracleConn { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            oracleConn = new OracleConnection("Data Source=10.3.0.227;User Id=Sheun;Password=1111;");
+            try
+            {
+                oracleConn.Open();
+            }
+            catch (Exception e)
+            {
+                oracleConn.Close();
+                Close();
+            }
+        }
+
+        private void Button_Click_Login(object sender, RoutedEventArgs e)
+        {
+            OracleCommand com = new OracleCommand();
+            com.Connection = oracleConn;
+            com.CommandText = "BEGIN PROVER('" + Login.Text + "','" + Password.Text + "') END";
+            var res = com.ExecuteScalar();
+
+            if ((bool)res == true)
+            {
+            }
+            else
+            {
+            }
+
+        }
+
+        private void Button_Click_Registr(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text != null && Password.Text != null)
+                try
+                {
+                    OracleCommand com = new OracleCommand("INSERT INTO OPERATOR_(NAME,PSWD) VALUES('" + Login.Text + "','"
+                        + Password.Text + "')", oracleConn);
+                    var res = com.ExecuteReader();
+                    //while (res.Read())
+                    //{
+                    //    Console.WriteLine(res[1].ToString());
+                    //}
+                }
+                catch { }
+
+        }
+    }
+}
---
+++ Map_Exam/Map_Exam/Map.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsPresentation;
+//test
+namespace Map_Exam
+{
+    /// <summary>
+    /// Логика взаимодействия для Map.xaml
+    /// </summary>
+    public partial class Map : Window
+    {
+        List<PointLatLng> Lpll = new List<PointLatLng>();
+        public Map()
+        {
+            InitializeComponent();
+        }
+
+        private void gmap_Loaded(object sender, RoutedEventArgs e)
+        {
+            gmap.MapProvider = BingMapProvider.Instance;
+            gmap.SetPositionByKeywords("Paris, France");
+        }
+
+        private void gmap_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(gmap);
+            PointLatLng pll = gmap.FromLocalToLatLng((int)p.X, (int)p.Y);
+            Lpll.Add(pll);
+            GMapMarker gmm = new GMapMarker(pll);
+            gmm.Shape = new Ellipse() { Width = 10, Height = 10, Fill = Brushes.Red };
+            gmap.Markers.Add(gmm);
+        }
+
+        private void Create_Map_Click(object sender, RoutedEventArgs e)
+        {
+            RoutingProvider rp = gmap.MapProvider as RoutingProvider;
+            List<PointLatLng> ps = new List<PointLatLng>();
+            for (int i = 0; i < Lpll.Count - 1; i++)
+            {
+                MapRoute r = rp.GetRoute(Lpll[i], Lpll[i + 1], false, true, 13);
+                ps.AddRange(r.Points);
+            }
+            GMapRoute mapRoute = new GMapRoute(ps);
+            gmap.Markers.Add(mapRoute);
+            gmap.ZoomAndCenterMarkers(13);
+        }
+
+        bool b = false;
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (b)
+            {
+                MenuLeft.Width = new GridLength(150);
+                MenuRight.Width = new GridLength(150);
+            }
+            else
+            {
+                MenuLeft.Width = new GridLength(0);
+                MenuRight.Width = new GridLength(0);
+            }
+            b = !b;
+        }
+
+        private void ListRoute_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ListDot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DotName.Text = "";
+            DotDescript.Text = "";
+            DotCoord.Text = "";
+            DotImage.Text = "";
+        }
+
+        private void But_Like_Click(object sender, RoutedEventArgs e)
+        {
+            if (true)
+                But_Like.Content = "Дизлайк";
+            else
+                But_Like.Content = "Лайк";
+        }
+
+        private void But_Comment_Click(object sender, RoutedEventArgs e)
+        {
+            Route_Descript.Text = "";
+        }
+
+        private void But_Complaite_Click(object sender, RoutedEventArgs e)
+        {
+            Route_Descript.Text = "";
+        }
+
+        private void But_Filtr_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
